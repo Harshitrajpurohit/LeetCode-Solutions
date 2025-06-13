583e48c49b6464ace5984a0ec635d707e95869ef
Harshitrajpurohit <indianhars10@gmail.com>
2025-06-13 10:58:42 +0530

    Day 7

--- Majority-Element.cpp
+++ Majority-Element.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-
-int majorityElement(vector<int> arr){
+// TC-> O(n log(n)) + O(n); SC-> O(n)
+int majorityElementByMap(vector<int> arr){
     map<int,int> freq;
 
     for(int i=0;i<arr.size();i++){
@@ -25,9 +25,41 @@ int majorityElement(vector<int> arr){
 
 }
 
+// TC-> O(n) SC-> O(n)
+int majorityElement(vector<int> arr){
+    
+    int cnt=0;
+    int ele;
+
+    for(int i=0;i<arr.size();i++){
+        if(cnt==0){
+            cnt=1;
+            ele = arr[i];
+        }else if(arr[i]==ele){
+            cnt++;
+        }else{
+            cnt--;
+        }
+    }
+    int cnt1=0;
+    for(int i=0;i<arr.size();i++){
+        if(ele == arr[i]){
+            cnt1++;
+        }
+    }
+
+    if(cnt1 > arr.size()/2){
+        return ele;
+    }
+    return -1;
+}
+
+
+
 int main(){
     vector<int> arr = {2,2,1,1,1,2,2};
 
+    int ans = majorityElementByMap(arr);
     int ans = majorityElement(arr);
     cout<<ans<<endl;
 }
